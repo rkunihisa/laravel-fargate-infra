8129296f22ad7348df4a89bdb007ce20d6dbaa77
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-08-05 23:56:40 +0900

    Add nginx module configuration for ECR repository

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -27,3 +27,8 @@ resource "aws_ecr_lifecycle_policy" "nginx" {
   )
   repository = aws_ecr_repository.nginx.name
 }
+
+module "nginx" {
+  source = "../../../../modules/ecr"
+  name   = "example-prod-foobar-nginx"
+}
